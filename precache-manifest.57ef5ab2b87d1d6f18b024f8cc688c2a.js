6d3aaa8a53d0de24275018c4018caefe9dcb5ac6
Tiago Marques <tmarques@dlocal.com>
2021-09-06 15:08:23 -0300

    Updates

---
+++ precache-manifest.57ef5ab2b87d1d6f18b024f8cc688c2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9427d08d8aa705e79af9",
+    "url": "/static/css/main.51955e96.chunk.css"
+  },
+  {
+    "revision": "9427d08d8aa705e79af9",
+    "url": "/static/js/main.5bb379cf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "aeb631cc051306bb36f7",
+    "url": "/static/js/2.1cdd8c79.chunk.js"
+  },
+  {
+    "revision": "8bd1a6f3405f3d41175c74398253c444",
+    "url": "/static/media/pluralsightiq.8bd1a6f3.png"
+  },
+  {
+    "revision": "8dd827eb9427592810912968aedce0b7",
+    "url": "/static/media/pattern_2.8dd827eb.png"
+  },
+  {
+    "revision": "a15fc103048e9672c32d285b101e3a97",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1dcb1370a36d066bd36cfc540d898948.js"
+  "/precache-manifest.57ef5ab2b87d1d6f18b024f8cc688c2a.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
